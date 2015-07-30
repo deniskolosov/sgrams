2186da510417fe7cc0c1c9461656c4956c1ec4e2
Denis Kolosov <denis.kolosov@gmail.com>
2015-07-30 18:47:17 +0300

    frontend alpha version

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.Static("/static", "./static")
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
 		})
 	})
-	router.Run(":8080")
+	router.Run(":8088")
 }
